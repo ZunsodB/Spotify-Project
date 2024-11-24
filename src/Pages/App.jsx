7a9065644cd37366ec969f3807b3212242749db8
ZunsodB <s21c118b@nmct.edu.mn>
2024-11-25 01:48:26 +0800

    header nemev

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -5,19 +5,19 @@ import MusicPlayer from "../Layouts/MusicPlayer";
 
 export default function App() {
   return (
-    <div className="h-screen bg-black flex flex-col">
-      <header className="h-[9%] w-full bg-black">
+    <div className="h-screen bg-black flex flex-col font-sans">
+      <header className="h-[57px] w-full bg-black">
         <Header />
       </header>
       <main className="flex flex-row flex-1 gap-2 p-2">
-        <aside className="flex bg-[#181818] rounded-lg w-[25%]">
+        <aside className="flex bg-[rgba(18,18,18)] rounded-lg w-[29.5%]">
           <Sidebar />
         </aside>
-        <section className="flex-1 bg-[#181818] rounded-lg">
+        <section className="flex-1 bg-[rgba(18,18,18)] rounded-lg">
           <Display />
         </section>
       </main>
-      <footer className="h-[10%] w-full bg-black">
+      <footer className="h-[80px] w-full bg-black">
         <MusicPlayer />
       </footer>
     </div>
