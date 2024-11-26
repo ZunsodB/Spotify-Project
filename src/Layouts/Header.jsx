13fb752d4350cd30afebb9e2e77a67f1f63c8bf5
ZunsodB <s21c118b@nmct.edu.mn>
2024-11-26 22:41:38 +0800

    home tex hovering

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -1,6 +1,7 @@
 import logo from '../assets/images/Logo(shadowify).png'
 import home from '../assets/images/home-icon.png'
 import search from '../assets/images/search-icon.png'
+
 const Header = () => {
   return (
     <div className="flex flex-row justify-between text-white text-center h-full pt-2">
@@ -10,22 +11,25 @@ const Header = () => {
           className="h-full w-36 object-cover pb-2" />
       </div>
       <div className="flex flex-row w-[70%] mb-0.5">
-          <div className="flex h-full w-[48px] justify-center items-center rounded-full bg-[rgba(31,31,31)] ml-[2.3%]">
-            <img  
-              src={home} alt="Home" 
-              className="h-7 w-7 object-cover "/>  
+        <div className="group flex flex-col items-center h-full w-[48px] justify-center rounded-full bg-[rgba(31,31,31)] ml-[2.3%] transform transition-transform duration-300 hover:scale-105 relative">
+          <img  
+            src={home} alt="Home" 
+            className="h-7 w-7 object-cover" />  
+          {/* Home text appearing hover effect */}
+          <div className="absolute text-white bg-[rgba(31,31,31)] px-2 py-1 rounded-md text-sm opacity-0 transition-opacity duration-300 group-hover:opacity-100 top-[57px]">
+            Home
           </div>
-          <div className="flex flex-row items-center h-full w-[475px] bg-[rgba(31,31,31)] rounded-full ml-2">
-            <img 
-              src={search} alt="search" 
-              className="h-7 w-7 mx-3"/>
-            <p className="font-sans text-[#b3b3b3] text-md font-normal tracking-wide mt-1">
-              What do you want to play?
-            </p>
-          </div>
-          <div className="right">
-
-          </div>
+        </div>
+        <div className="flex flex-row items-center h-full w-[475px] bg-[rgba(31,31,31)] rounded-full ml-2">
+          <img 
+            src={search} alt="search" 
+            className="h-7 w-7 mx-3"/>
+          <p className="font-sans text-[#b3b3b3] text-md font-normal tracking-wide mt-1">
+            What do you want to play?
+          </p>
+        </div>
+        <div className="right">
+        </div>
       </div>
     </div>
   )
